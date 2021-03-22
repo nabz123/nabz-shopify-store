18e1b1373c9716a0ca16bd3f5fb492e6a151b961
nabz123 <mohanr1@student.op.ac.nz>
2021-03-23 11:37:12 +1300

    test

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,7 @@ const ContactForm = () => (
   <>
     <SEO title="Contact" />
     <h1>Send Us a Message</h1>
-    <form name="contact" method="POST" netlify>
+    <form name="contact" method="POST" data-netlify="true">
         <input type="hidden" name="form-name" value="contact" />
         <p>
             <label>Your Name: <input type="text" name="name" /></label>   
